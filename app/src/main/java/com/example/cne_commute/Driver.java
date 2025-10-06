3a813c3dff7d81710157c69be9883a340de16d00
rin <pjdsena@gmail.com>
2025-10-06 15:27:50 +0800

    Revert "Operator, Franchise card, Franchise details, Add driver, Assign driver, Assigned Driver Info, Assigned Driver History"

--- app/src/main/java/com/example/cne_commute/Driver.java
+++ app/src/main/java/com/example/cne_commute/Driver.java
@@ -4,47 +4,8 @@ import com.google.gson.annotations.SerializedName;
 
 public class Driver {
     @SerializedName("driver_id")
-    public String driverId;
+    public String driverID;
 
     @SerializedName("password")
     public String password;
-
-    @SerializedName("driver_name_clean")
-    private String fullName;
-
-    @SerializedName("first_name")
-    public String firstName;
-
-    @SerializedName("middle_name")
-    public String middleName;
-
-    @SerializedName("last_name")
-    public String lastName;
-
-    @SerializedName("suffix")
-    public String suffix;
-
-    @SerializedName("address")
-    public String address;
-
-    @SerializedName("contact_num")
-    public String contactNum;
-
-    @SerializedName("license_num")
-    public String licenseNum;
-
-    @SerializedName("license_expiration")
-    public String licenseExpiration;
-
-    @SerializedName("license_restriction")
-    public String licenseRestriction;
-
-    public String getDriverId() {
-        return driverId;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
 }
